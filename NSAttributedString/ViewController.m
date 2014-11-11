afa5aaa913b297e83d9ea8790d873092f5d46f6f
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-11-11 09:45:17 -0200

    Initial commit

--- NSAttributedString/ViewController.m
+++ NSAttributedString/ViewController.m
@@ -14,9 +14,18 @@
 
 @implementation ViewController
 
+@synthesize textView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *sourceHtml=@"<br><br><br><br><h3><center>Title</center></h3><br><br><center>Hello World!</center><br>";
+    
+    
+    NSString *htmlString = sourceHtml;
+    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
+    self.textView.attributedText = attributedString;
 }
 
 - (void)didReceiveMemoryWarning {
